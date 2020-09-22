acde98d9ef99c26a1d542c1556d27794ce901ba1
RomBik17 <r.rafalyuk@gmail.com>
2020-09-22 16:08:24 +0300

    fusion functions

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ int main()
 {
     __m256 evens    = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
     __m256 odds     = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
-    __m256 vec     = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+    __m256 vec      = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
 
     initialization_and_sub();
     multiply_and_add(evens, odds, vec);
