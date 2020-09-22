cdfeca98d579181228e89d0b38caa6b0949c8042
RomBik17 <r.rafalyuk@gmail.com>
2020-09-22 14:51:46 +0300

    mask vector

--- main.cpp
+++ main.cpp
@@ -36,17 +36,33 @@ int main()
 
     /* Compute the difference between the two vectors */
     __m256 result = _mm256_sub_ps(evens, odds);
-
+    //called a function
     __m256 c = multiply_and_add(evens, odds, result);
 
     /* Display the elements of the result vector */
     float* f = (float*)&result;
     printf("%f %f %f %f %f %f %f %f\n",
            f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
-
     float* d = (float*)&c;
     printf("%f %f %f %f %f %f %f %f\n",
            d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
+
+
+
+    int int_array[8] = {100, 200, 300, 400, 500, 600, 700, 800};
+
+    /* Initialize the mask vector */
+    __m256i mask = _mm256_setr_epi32(-20, -72, -48, 9, -100, -3, 5, 8);
+
+    /* Selectively load data into the vector */
+    __m256i result2 = _mm256_maskload_epi32(int_array, mask);
+
+    /* Display the elements of the result vector */
+    int* res = (int*)&result2;
+    printf("%d %d %d %d %d %d %d %d\n",
+           res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7]);
+
+
 
     return 0;
 }
